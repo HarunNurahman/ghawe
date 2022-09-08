2638111a1d210b3ac8e918a05d76945f9290298c
Harun Nurahman <harun.nurrahman@hotmail.com>
2022-09-08 21:32:06 +0700

    Slicing UI Detail Page, Fixing Bug on Detail Page

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -1,136 +1,135 @@
 import 'package:flutter/material.dart';
+import 'package:ghawe/pages/widgets/job_info_item.dart';
 import 'package:ghawe/shared/style.dart';
 
 class DetailPage extends StatelessWidget {
   const DetailPage({Key? key}) : super(key: key);
 
+  PreferredSizeWidget _appbar() {
+    return AppBar(
+      automaticallyImplyLeading: false,
+      backgroundColor: kWhiteColor,
+      centerTitle: true,
+      elevation: 0,
+      title: Text(
+        'Detail Pekerjaan',
+        style: blackTextStyle.copyWith(
+          fontSize: 18,
+          fontWeight: semiBold,
+        ),
+      ),
+      actions: [
+        Padding(
+          padding: EdgeInsets.only(right: defaultMargin),
+          child: Icon(
+            Icons.bookmark_outline,
+            color: kPrimaryColor,
+            size: 24,
+          ),
+        ),
+      ],
+    );
+  }
+
+  // header widget (company logo, job title, company name)
+  Widget _header() {
+    return Center(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            width: 100,
+            height: 100,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                  'assets/images/img_tokopedia.png',
+                ),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          const SizedBox(height: 20),
+          Text(
+            'Mobile Developer',
+            textAlign: TextAlign.center,
+            overflow: TextOverflow.ellipsis,
+            style: blackTextStyle.copyWith(
+              fontSize: 20,
+              fontWeight: semiBold,
+            ),
+          ),
+          Text(
+            'Tokopedia',
+            textAlign: TextAlign.center,
+            style: blackTextStyle,
+          )
+        ],
+      ),
+    );
+  }
+
+  // body content widget
+  Widget _body() {
+    Widget _jobInfo() {
+      return Column(
+        children: [
+          Row(
+            children: [
+              JobInfoItem(
+                iconUrl: Icons.attach_money_outlined,
+                title: 'IDR 3.500.000',
+              ),
+              JobInfoItem(
+                iconUrl: Icons.access_time,
+                title: 'Full-Time',
+              ),
+            ],
+          ),
+          const SizedBox(height: 10),
+          Row(
+            children: [
+              JobInfoItem(
+                iconUrl: Icons.work,
+                title: '1 - 3 Tahun',
+              ),
+              JobInfoItem(
+                iconUrl: Icons.location_on,
+                title: 'Daerah Istimewa Yogyakarta',
+              ),
+            ],
+          ),
+        ],
+      );
+    }
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        _jobInfo(),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    PreferredSizeWidget _appbar() {
-      return AppBar(
-        backgroundColor: kWhiteColor,
-        automaticallyImplyLeading: false,
-        elevation: 0,
-        centerTitle: true,
-        title: Text(
-          'Detail Pekerjaan',
-          style: blackTextStyle.copyWith(
-            fontSize: 16,
-            fontWeight: semiBold,
-          ),
-        ),
-        actions: [
-          Padding(
-            padding: EdgeInsets.only(right: defaultMargin),
-            child: Icon(
-              Icons.bookmark_outline,
-              color: kPrimaryColor,
-            ),
-          ),
-        ],
-        toolbarHeight: 90,
-      );
-    }
-
-    // header widget (company logo, job title, company)
-    Widget _header() {
-      return Center(
-        child: Column(
-          children: [
-            Image.asset(
-              'assets/images/img_tokopedia.png',
-              width: 128,
-            ),
-            const SizedBox(height: 20),
-            Text(
-              'Mobile Developer',
-              style: blackTextStyle.copyWith(
-                fontSize: 18,
-                fontWeight: semiBold,
-              ),
-            ),
-            const SizedBox(height: 4),
-            Text(
-              'Tokopedia',
-              style: blackTextStyle,
-            )
-          ],
-        ),
-      );
-    }
-
-    // main content widget
-    Widget _body() {
-      // basic jobinfo widget (salary, location, job type, experience)
-      Widget _jobInfo() {
-        return Row(
-          children: [
-            Expanded(
-              child: Row(
-                children: [
-                  Icon(
-                    Icons.attach_money_rounded,
-                    color: kPrimaryColor,
-                  ),
-                  Text(
-                    'IDR 3.500.000',
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                    style: blackTextStyle.copyWith(
-                      fontWeight: semiBold,
-                    ),
-                  )
-                ],
-              ),
-            ),
-            Expanded(
-              child: Row(
-                children: [
-                  Icon(
-                    Icons.attach_money_rounded,
-                    color: kPrimaryColor,
-                  ),
-                  Text(
-                    'IDR 3.500.000',
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                    style: blackTextStyle.copyWith(
-                      fontWeight: semiBold,
-                    ),
-                  )
-                ],
-              ),
-            )
-          ],
-        );
-      }
-
-      return Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          _jobInfo(),
-        ],
-      );
-    }
-
     return Scaffold(
       backgroundColor: kWhiteColor,
       appBar: _appbar(),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.only(
-            left: defaultMargin,
-            right: defaultMargin,
-            top: defaultMargin,
-            bottom: defaultMargin,
-          ),
-          child: Column(
-            children: [
-              _header(),
-              SizedBox(height: defaultMargin),
-              _body(),
-            ],
-          ),
+      body: Padding(
+        padding: EdgeInsets.only(
+          left: defaultMargin,
+          right: defaultMargin,
+          top: defaultMargin,
+          bottom: defaultMargin,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _header(),
+            SizedBox(height: defaultMargin),
+            _body(),
+          ],
         ),
       ),
     );
---
+++ lib/pages/widgets/job_info_item.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:ghawe/shared/style.dart';
+
+class JobInfoItem extends StatelessWidget {
+  final IconData iconUrl;
+  final String title;
+
+  const JobInfoItem({
+    Key? key,
+    required this.iconUrl,
+    required this.title,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Row(
+        children: [
+          Icon(
+            iconUrl,
+            size: 24,
+            color: kPrimaryColor,
+          ),
+          const SizedBox(width: 6),
+          Expanded(
+            child: Text(
+              title,
+              overflow: TextOverflow.ellipsis,
+              style: blackTextStyle.copyWith(
+                fontWeight: semiBold,
+              ),
+              maxLines: 2,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
